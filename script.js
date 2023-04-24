021f7cde93e86dd2170b10d30aa724d049d4b2db
bokolmeijer <99927404+bokolmeijer@users.noreply.github.com>
2023-04-24 21:42:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,18 +6,6 @@ let iframe = document.getElementById("pen");
 let penLink = document.getElementById("penlink");
 let links = document.getElementsByTagName("a");
 
-let frames = [
-  "aboutme.html",
-  "https://cdpn.io/cobra_winfrey/debug/wvGyKEY",
-  "https://codepen.io/cobra_winfrey/debug/OJVJJoj",
-  "https://cdpn.io/cobra_winfrey/debug/YzXOBEN",
-  "https://codepen.io/cobra_winfrey/debug/qBZWVmO",
-  "https://codepen.io/cobra_winfrey/debug/eYOXOdB",
-  "https://codepen.io/cobra_winfrey/debug/OJXJeod",
-  "https://codepen.io/cobra_winfrey/debug/PoqVQRq",
-  "https://cdpn.io/cobra_winfrey/debug/qgEGMZ",
-  "https://codepen.io/cobra_winfrey/debug/RwWYGxj"
-];
 
 // Load iFrames on demand & remove after modal is closed to reduce weight & smooth out transitions
 
@@ -145,3 +133,127 @@ function refresh() {
 }
 
 window.addEventListener("resize", refresh);
+
+const magicAreas = [...document.querySelectorAll(".c-magic-area")];
+
+const getAreaDetails = (area) => {
+  const width = area.clientWidth;
+  const height = area.clientHeight;
+
+  const position = area.getBoundingClientRect();
+  const top = position.top + window.scrollY;
+  const left = position.left;
+  console.log(position.top);
+  return {
+    left,
+    height,
+    top,
+    width
+  };
+};
+
+const setTweenArea = (link, magicArea) => {
+  const { left, height, top, width } = getAreaDetails(link);
+
+  gsap.set(magicArea, {
+    top,
+    left,
+    width,
+    height
+  });
+};
+
+const tweenMagicArea = (target, magicArea) => {
+  const { left, height, top, width } = getAreaDetails(target);
+
+  gsap.to(magicArea, 0.5, {
+    left,
+    top,
+    width,
+    height,
+    ease: Power3.easeInOut
+  });
+};
+
+const getMagicActiveElement = (links) => {
+  return links.filter((link) => {
+    return (
+      link.classList.contains("is-magic-active") ||
+      link.getAttribute("aria-current") === "page"
+    );
+  });
+};
+
+const moveMagicArea = (links, magicArea, isTweenBack) => {
+  const magicActiveElement = getMagicActiveElement(links);
+
+  links.map((link) => {
+    link.addEventListener("mouseenter", function (e) {
+      tweenMagicArea(e.target, magicArea);
+    });
+
+    link.addEventListener("focus", function (e) {
+      tweenMagicArea(e.target, magicArea);
+    });
+
+    if (isTweenBack && magicActiveElement.length) {
+      link.addEventListener("mouseleave", function (e) {
+        tweenMagicArea(magicActiveElement[0], magicArea);
+      });
+
+      link.addEventListener("focusout", function (e) {
+        tweenMagicArea(magicActiveElement[0], magicArea);
+      });
+    }
+  });
+};
+
+const setMagic = (links, magicArea) => {
+  // check if .is-magic-active || aria-current="page"
+  const magicActiveElement = getMagicActiveElement(links);
+
+  if (magicActiveElement.length) {
+    setTweenArea(magicActiveElement[0], magicArea);
+  } else {
+    setTweenArea(links[0], magicArea);
+  }
+};
+
+// const onResize = (links, magicArea) => {
+//   setMagic(links, magicArea);
+// };
+
+const initMagic = ({ isResize } = { isResize: false }) => {
+  if (!magicAreas.length) return;
+
+  magicAreas.map((magicArea) => {
+    const targetMagicArea = magicArea.getAttribute("data-target-class");
+
+    const links = [...document.querySelectorAll(targetMagicArea)];
+
+    if (!links.length) return;
+
+    setMagic(links, magicArea);
+
+    if (!isResize) {
+      const isTweenBack = magicArea.getAttribute("data-tween-back") === "true";
+
+      moveMagicArea(links, magicArea, isTweenBack);
+    }
+  });
+};
+
+initMagic();
+
+window.addEventListener(
+  "resize",
+  _.throttle(function () {
+    initMagic({ isResize: true });
+  }, 100)
+);
+
+VanillaTilt.init(document.querySelector(".c-fe30__inner"), {
+  max: 20,
+  perspective: 1000,
+  speed: 300
+});
