504c91d0a065b19d8a4fcc4a2b77e76acec989f6
bokolmeijer <99927404+bokolmeijer@users.noreply.github.com>
2023-04-28 13:56:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -257,3 +257,16 @@ VanillaTilt.init(document.querySelector(".c-fe30__inner"), {
   perspective: 1000,
   speed: 300
 });
+
+// script for tab steps
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+  var href = $(e.target).attr('href');
+  var $curr = $(".process-model  a[href='" + href + "']").parent();
+
+  $('.process-model li').removeClass();
+
+  $curr.addClass("active");
+  $curr.prevAll().addClass("visited");
+});
+// end  script for tab steps
